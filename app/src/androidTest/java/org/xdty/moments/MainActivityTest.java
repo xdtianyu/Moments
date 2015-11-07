6bc319a29eab24df1baa13351c991af2e677bceb
tianyu <xdtianyu@gmail.com>
2015-11-07 17:07:25 +0800

    add sample test

--- app/src/androidTest/java/org/xdty/moments/MainActivityTest.java
+++ app/src/androidTest/java/org/xdty/moments/MainActivityTest.java
@@ -12,6 +12,7 @@ import org.xdty.moments.activity.MainActivity_;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Created by ty on 15-11-4.
@@ -25,6 +26,16 @@ public class MainActivityTest {
     @Test
     public void shouldBeAbleLaunchMainScreen() {
         onView(withId(R.id.swipe_refresh_layout)).check(ViewAssertions.matches(isDisplayed()));
+    }
+
+    @Test
+    public void testUserNameExists() {
+        try {
+            Thread.sleep(10*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.username)).check(ViewAssertions.matches(withText("tianyu")));
     }
 }
 
